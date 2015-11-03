7a984ada19e14206c0ed83f84393bb907d281bff
Pierce Darragh <pierce.darragh@gmail.com>
2015-11-03 14:43:03 -0700

    Filled out cell implementation
    Cell class has implementation now. Woohoo!

---
+++ TheSpritening/cell.cpp
@@ -0,0 +1,44 @@
+/*
+ * cell.cpp
+ *
+ * Implementation for the Cell class. Cells are what become the pixels in a
+ * sprite animation. Cells have position and color.
+ */
+
+#include "cell.h"
+
+/*
+ * Constructors
+ */
+
+Cell::Cell()
+{
+    position = QPoint(0, 0);        // (0, 0)
+    color    = QColor(0, 0, 0, 0);  // black
+}
+
+Cell::Cell(QPoint _position)
+{
+    position = _position;
+    color    = QColor(0, 0, 0, 0);  // black
+}
+
+Cell::Cell(QPoint _position, QColor _color)
+{
+    position = _position;
+    color    = _color;
+}
+
+/*
+ * Methods
+ */
+
+QPoint Cell::getPosition()
+{
+    return position;
+}
+
+QColor Cell::getColor()
+{
+    return color;
+}
