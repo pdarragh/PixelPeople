a15f825595c92abf14437d1a1b83efa899d60181
Pierce Darragh <pierce.darragh@gmail.com>
2015-11-06 17:06:12 -0700

    reset to 4d2b4395f5e57185ed6608135d964ca90fab8560

--- TheSpritening/mainwindow.h
+++ TheSpritening/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPushButton>
+#include <QSlider>
 
 #include "canvas.h"
 
@@ -16,21 +18,17 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
      Canvas * scene;
-     QColor color;
-     void iGotClicked(QPointF point);
-     enum class Tools {Eraser, Pencil, Rotate, Mirror};
     ~MainWindow();
+
+private slots:
+     void fullPreviewButtonReleased();
+     void fpsValueChanged(int value);
+     void pplayButtonReleased();
+     void pskipButtonReleased();
+     void pbackButtonReleased();
 
 private:
     Ui::MainWindow *ui;
-
-public slots:
-    void clearPushed();
-    void addFramePushed();
-
-private slots:
-    void on_preview_released();
-    void on_colorButton_clicked();
 };
 
 #endif // MAINWINDOW_H
